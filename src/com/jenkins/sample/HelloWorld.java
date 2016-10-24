938ac36364061cac3713b23cb8eaaf50be558f3d
Karthik Kiran <kkiran@ip-10-10-192-166.eu-west-1.compute.internal>
2016-10-24 16:19:00 -0400

    modified print statement

--- src/com/jenkins/sample/HelloWorld.java
+++ src/com/jenkins/sample/HelloWorld.java
@@ -2,7 +2,7 @@ package com.jenkins.sample;
 
 public class HelloWorld {
 	public void print(){
-		System.out.println("Hello World");
+		System.out.println("Hello World Jenkins");
 	}
 	public static void main(String[] args){
 		HelloWorld hw = new HelloWorld();
